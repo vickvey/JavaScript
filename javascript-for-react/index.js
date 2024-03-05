7fe2c45e834d9b2478ba7e800d03ce40825a8c36
vickvey <vivek22@iiserb.ac.in>
2024-03-05 10:05:21 +0530

    added react basics

---
+++ javascript-for-react/index.js
@@ -0,0 +1,9 @@
+// Using normal way to define functions
+export default function doSomething_wa() {
+    //
+}
+
+// Defining functions using arrow functions
+export const doSomething = () => {
+    //
+}
