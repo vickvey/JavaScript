9fd0037ffc48f9a0fe22e4f486cb308b618dfe09
vickvey <vickvey0303@gmail.com>
2024-08-26 16:18:14 +0530

    added basic js

---
+++ Modern-JS/tut1/script.js
@@ -0,0 +1,10 @@
+// Q1
+const button = document.createElement('button');
+button.innerText = 'click me';
+button.style.background = 'red';
+button.style.color = 'white';
+
+document.querySelector('body').prepend(button);
+
+// Q2
+let para = document.querySelector('p');
